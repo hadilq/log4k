0d21c254871c1cbb54e85fb0fa4228c36063587a
Hadi <hadilashkari@gmail.com>
2020-08-28 17:00:17 +0200

    Bump up to 2.1.0

--- build-plugin/src/main/java/com/github/hadilq/build/plugin/BuildPlugin.kt
+++ build-plugin/src/main/java/com/github/hadilq/build/plugin/BuildPlugin.kt
@@ -31,7 +31,7 @@ private const val VERSION_JUNIT = "4.12"
 const val VERSION_JACOCO = "0.8.5"
 
 const val GROUP_ID = "com.github.hadilq"
-const val LIB_VERSION = "2.0.0"
+const val LIB_VERSION = "2.1.0"
 
 const val VERSION_COMPILE_SDK = 29
 const val VERSION_MIN_SDK = 15
